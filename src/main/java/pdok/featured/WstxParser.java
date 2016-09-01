b42bf78b5d75f29aff959e12f75aa22cf5f690f2
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-09-01 16:22:46 +0200

    feat: moves featured.feature ns to shared
    So we can share feature functionality between featured and
    featured-to-extracts.

---
+++ src/main/java/pdok/featured/WstxParser.java
@@ -0,0 +1,30 @@
+package pdok.featured;
+
+import com.ctc.wstx.sax.WstxSAXParserFactory;
+import com.ctc.wstx.stax.WstxInputFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import org.geotools.xml.Configuration;
+import org.geotools.xml.Parser;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+
+public class WstxParser extends Parser {
+
+    private final WstxSAXParserFactory factory;
+
+    public WstxParser(Configuration config) throws SAXNotRecognizedException, SAXNotSupportedException {
+        super(config);
+        WstxInputFactory inputFactory = new WstxInputFactory();
+        inputFactory.configureForSpeed();
+
+        factory = new WstxSAXParserFactory(inputFactory);
+
+    }
+
+    @Override
+    protected SAXParser parser(boolean validate) throws ParserConfigurationException, SAXException {
+        return factory.newSAXParser();
+    }
+}
