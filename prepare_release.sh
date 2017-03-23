4c3a036509e7e3e07dea39a312844c4c1858a006
Bas Duineveld <Bas.Duineveld@kadaster.nl>
2017-03-23 13:57:30 +0100

    Update release script

--- prepare_release.sh
+++ prepare_release.sh
@@ -6,19 +6,16 @@ printf "Release version? "
 read release_version
 
 mvn -q versions:set -DnewVersion=$release_version
-
 git add pom.xml
-git commit -q -m "chore: prepare release $release_version"
-
-git tag -a featured-shared-$release_version -m "release $release_version"
+git commit -q -m "Prepare release $release_version"
+git tag -a featured-shared-$release_version -m "Release $release_version"
 
 printf "New development version? "
 read dev_version
-mvn -q versions:set -DnewVersion=$dev_version
 
+mvn -q versions:set -DnewVersion=$dev_version
 git add pom.xml
-git commit -q -m "chore: new release cycle $dev_version [ci skip]"
+git commit -q -m "New release cycle $dev_version [ci skip]"
 
 mvn -q versions:commit
-
 git push --follow-tags
